a3fa0b9fbd635b1efc7bc2298f12f4d3aa4c6743
richardYang <15618118216@163.com>
2024-06-10 19:54:39 +0800

    update blog

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App () {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Richard13088 My personal Blog</p>
+        <p>我是小游的同学，你好</p>
         <a
           className="App-link"
           href="https://reactjs.org"
